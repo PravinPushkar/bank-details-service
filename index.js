1dcf3a74ba49382786aca149d7ae9644f7157804
Pravin Pushkar <pravin.pratap.pushkar@sap.com>
2019-09-22 23:07:32 +0530

    initial commit-10

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const _ = require('lodash');
 const {pool} = require('./config');
 
 const app = express();
@@ -8,34 +9,32 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
-
+const defaultLimit = {
+  'limit':20
+};
 const getBankDetails = (request,response) => {
   let ifscCode = request.params.ifsc;
-  pool.query('SELECT * from bank_branches where ifsc=$1',[ifscCode], (error,results) => {
+  let combinedObj = _.assign(defaultLimit,request.query);
+  let limit = combinedObj.limit;
+  let offset = _.isUndefined(combinedObj.offset)?0:combinedObj.offset;
+  pool.query('SELECT * from bank_branches where ifsc=$1 LIMIT $2 OFFSET $3',[ifscCode,limit,offset], (error,results) => {
     if(error) throw error;
     response.status(200).json(results.rows);
   });
 }
 
 const getBranchDetails = (request,response) => {
-  console.log(JSON.stringify(request.query));
-  let bankName = request.query.bankName;
+  let bankName = request.params.bankName;
   bankName=bankName.toLowerCase();
-  let city = request.query.city;
+  let city = request.params.city;
   city=city.toLowerCase();
-  console.log("===============");
-  console.log(bankName + ":::" + city);
-  console.log("===============");
-  if(bankName.length!=0 && city.length!=0) {
-    pool.query('SELECT * from bank_branches where LOWER(bank_name)=$1 and LOWER(city)=$2',[bankName,city], (error,results) => {
-      if(error) throw error;
-      response.status(200).json(results.rows);
-    });
-  }
-  else{
-    throw new Error("Please provice Bank Name and city name");
-  }
-  
+  let combinedObj = _.assign(defaultLimit,request.query);
+  let limit = combinedObj.limit;
+  let offset = _.isUndefined(combinedObj.offset)?0:combinedObj.offset;
+  pool.query('SELECT * from bank_branches where LOWER(bank_name)=$1 and LOWER(city)=$2 LIMIT $3 OFFSET $4',[bankName,city,limit,offset], (error,results) => {
+    if(error) throw error;
+    response.status(200).json(results.rows);
+  });
 }
 
 app.route('/bankDetails/:ifsc').get(getBankDetails);
