21fbd196f333ff916e1db061a4b4f8139eb576b4
Pravin Pushkar <pravin.pratap.pushkar@sap.com>
2019-09-22 15:16:26 +0530

    initial commit-3

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
-const test = (request,response) => {
-  pool.query('SELECT name from banks where id=1', (error,results) => {
+const getBankDetails = (request,response) => {
+  let ifscCode = request.params.ifsc;
+  pool.query('SELECT * from bank_branches where ifsc=$1',[ifscCode], (error,results) => {
     if(error) throw error;
     response.status(200).json(results.rows);
   });
 }
 
-app.route('/test').get(test);
+app.route('/bankDetails/:ifsc').get(getBankDetails);
 
 app.listen(process.env.PORT,() => {
   console.log('server started');
