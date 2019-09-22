c4def88c7186e7e5edd2b729a4bff5b2bee50568
Pravin Pushkar <pravin.pratap.pushkar@sap.com>
2019-09-22 15:48:20 +0530

    initial commit-4

--- index.js
+++ index.js
@@ -17,7 +17,17 @@ const getBankDetails = (request,response) => {
   });
 }
 
+const getBranchDetails = (request,response) => {
+  let bankName = request.params.ifsc;
+  let city = request.params.city;
+  pool.query('SELECT * from bank_branches where bank_name=$1 and city=$2',[bankName,city], (error,results) => {
+    if(error) throw error;
+    response.status(200).json(results.rows);
+  });
+}
+
 app.route('/bankDetails/:ifsc').get(getBankDetails);
+app.route('/branchDetails/:bankName/:city').get(getBranchDetails);
 
 app.listen(process.env.PORT,() => {
   console.log('server started');
