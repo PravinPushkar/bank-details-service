262f08cf541422cb005593fe4d493090aecd0c10
Pravin Pushkar <pravin.pratap.pushkar@sap.com>
2019-09-24 13:50:40 +0530

    initial commit-15

--- ApiController.js
+++ ApiController.js
@@ -1,9 +1,11 @@
+const {pool} = require('./config');
+
 class ApiController {
   constructor() {}
 
   getBankDetails(ifscCode, limit, offset,cb) {
     pool.query('SELECT * from bank_branches where ifsc=$1 LIMIT $2 OFFSET $3',[ifscCode,limit,offset], (error,results) => {
-      return cb(error, result);
+      return cb(error, results);
     });
   }
   getBranchDetails() {
